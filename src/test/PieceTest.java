48daca75427c19da018d50cbb7d36ac6b0b362e2
emmanuelAron <emmanuel.aron@gmail.com>
2018-07-17 15:19:27 +0200

    modification PieceTest.java

--- src/test/PieceTest.java
+++ src/test/PieceTest.java
@@ -6,13 +6,24 @@ import org.junit.jupiter.api.Test;
 
 import echecs.Case;
 import echecs.Cavalier;
+import echecs.Tour;
 
 public class PieceTest {
 
 	//(cas ou les 8 cases d'arrivee appartiennent au plateau)
 	@Test
-	public void cavalier_a_8_deplacements_possibles_au_maximum() {
+	public void cavalier_au_centre_a_8_deplacements_possibles() {
 		Case c = new Case(4,4);
 		assertThat(Cavalier.deplacements(c).size()).isEqualTo(8);
+	}
+	@Test
+	public void cavalier_au_coin_de_l_echiquier() {
+		Case c = new Case(0,0);
+		assertThat(Cavalier.deplacements(c).size()).isEqualTo(2);
+	}
+	@Test
+	public void tour_au_centre_a_14_deplacements_possibles() {
+		Case c = new Case(4,4);
+		assertThat(Tour.deplacements(c).size()).isEqualTo(14);
 	}
 }
